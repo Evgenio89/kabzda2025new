8fea8b5208a030b6eb5a6bf241ddc3d54012a063
Evgenio89 <evgenio891@gmail.com>
2025-01-06 23:44:15 +0300

    01 kabzda

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
-
 import './App.css'
 
 function App() {
+    return (
+        <>
+            <div>
+                <Rating/>
+                <Accordion/>
+            </div>
+        </>
+    )
+}
 
-  return (
-    <>
-     <div>Hello World</div>
-    </>
-  )
+function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Accordion () {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+
+function Star () {
+    return (
+        <div>
+            star
+        </div>
+    )
 }
 
 export default App
